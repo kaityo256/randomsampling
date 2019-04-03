b7e6f344be272953706daedfc6362cdffe936348
H. Watanabe <kaityo@users.sourceforge.jp>
2019-04-03 11:52:51 +0900

    adds walker

---
+++ cumulative_sum.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include <algorithm>
+#include <random>
+#include <vector>
+
+struct cumulative_sum {
+  std::vector<double> c_sum;
+  std::uniform_real_distribution<double> ud;
+  cumulative_sum(std::vector<double> &list) {
+    double sum = 0.0;
+    for (auto v : list) {
+      c_sum.push_back(sum);
+      sum += v;
+    }
+    ud.param(std::uniform_real_distribution<double>::param_type(0, sum));
+  }
+  int search(double v) {
+    int imin = 0;
+    int imax = c_sum.size();
+    int index = (imin + imax) / 2;
+    while (imax != index && imin != index) {
+      if (c_sum[index] < v) {
+        imin = index;
+      } else {
+        imax = index;
+      }
+      index = (imin + imax) / 2;
+    }
+    return index;
+  }
+  int select(std::mt19937 &mt) {
+    double v = ud(mt);
+    return search(v);
+  }
+};
---
+++ walker.hpp
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <cstdint>
+#include <numeric>
+#include <random>
+#include <vector>
+
+class walker_alias {
+private:
+  unsigned int n;
+  unsigned int nb;
+  std::vector<int> index;
+  std::vector<double> prob;
+  std::vector<uint32_t> probi;
+  uint32_t int_max = ~static_cast<uint32_t>(0);
+
+public:
+  walker_alias(std::vector<double> &a) {
+    nb = 1;
+    while ((unsigned int)(1 << nb) <= a.size()) {
+      nb++;
+    }
+    n = (1 << nb);
+    index.resize(n, 0.0);
+    prob.resize(n, 0.0);
+    probi.resize(n, 0);
+    for (unsigned int i = 0; i < a.size(); i++) {
+      prob[i] = a[i];
+    }
+    double ave = std::accumulate(prob.begin(), prob.end(), 0.0) / prob.size();
+    for (auto &v : prob) {
+      v /= ave;
+    }
+    std::vector<int> small, large;
+    for (unsigned int i = 0; i < n; i++) {
+      if (prob[i] < 1.0) {
+        small.push_back(i);
+      } else {
+        large.push_back(i);
+      }
+      index[i] = i;
+    }
+    while (small.size() && large.size()) {
+      const int j = small.back();
+      small.pop_back();
+      const int k = large.back();
+      index[j] = k;
+      prob[k] = prob[k] - 1.0 + prob[j];
+      if (prob[k] < 1.0) {
+        small.push_back(k);
+        large.pop_back();
+      }
+    }
+    const int imax = int_max >> nb;
+    for (unsigned int i = 0; i < n; i++) {
+      probi[i] = static_cast<uint32_t>(prob[i] * imax);
+    }
+  }
+  int select(std::mt19937 &mt) {
+    uint32_t r = mt();
+    int k = r & (n - 1);
+    r = r >> nb;
+    if (r > probi[k]) {
+      return index[k];
+    } else {
+      return k;
+    }
+  }
+};
